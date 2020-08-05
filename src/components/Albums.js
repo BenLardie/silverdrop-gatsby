229eb239d2bc781b61e745c41a14b3e0ae766502
Ben Lardie <benlardie@gmail.com>
2020-08-05 15:22:42 -0400

    its getting slick up in here :nail_care:

--- src/components/Albums.js
+++ src/components/Albums.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import { useStaticQuery, graphql } from 'gatsby'
 import AlbumCover from './AlbumCover'
+import Slider from "react-slick";
 
 export default function Albums() {
-    const data = useStaticQuery(graphql`
+  const data = useStaticQuery(graphql`
     query albumsQuery   {
         allMarkdownRemark(filter: {fileAbsolutePath: {regex: "/markdown/albums/"}}) {
           edges {
@@ -29,19 +30,29 @@ export default function Albums() {
   `)
   const albumData = data.allMarkdownRemark.edges
 
-    return (
-        <section className='comedyAlbums'>
-          {albumData.map(album => console.log(album))}
-            {albumData.map((album, i=0) => {
-                const image = album.node.frontmatter.thumbnail.childImageSharp.fluid
-                const title = album.node.frontmatter.title
-                i++
-                console.log(image)
-                return(
-                    <AlbumCover image={ image } title={ title }key={i} />
-                )
-            })}
-        </section>
-    )
+  const setting = {
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    arrows: false,
+
+  }
+
+  return (
+    <div className='slideWrapper'>
+    <Slider {...setting}>
+      {albumData.map((album, i = 0) => {
+        const image = album.node.frontmatter.thumbnail.childImageSharp.fluid
+        const title = album.node.frontmatter.title
+        i++
+        return (
+          <AlbumCover image={image} title={title} key={i} />
+        )
+      })}
+    </Slider>
+    </div>
+  )
 }
 
