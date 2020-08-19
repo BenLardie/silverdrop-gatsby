67b25e9c2557122ba234404b983c7410c54c870c
Ben Lardie <benlardie@gmail.com>
2020-08-18 21:36:43 -0400

    added demo reel

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import logo from '../../static/img/SDM_white.png'
+import sample from '../../static/img/2019_SDM_Promo_2019_30sec.mp4'
 import '../styles/hero.sass'
 
 
@@ -7,7 +8,10 @@ import '../styles/hero.sass'
 export default function Hero() {
     return (
         <section className='heroBox'>
-            <img src={logo} alt='logo' className='logo'/>
+            <img src={logo} alt='logo' className='logo' />
+            <video className='videoTag' autoPlay loop muted>
+                <source src={sample} type='video/mp4' />
+            </video>
         </section>
     )
 }
