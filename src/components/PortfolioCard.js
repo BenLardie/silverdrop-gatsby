1dfa7c20b750ca67ed88cc642d1db512ede4bf6c
Ben Lardie <benlardie@gmail.com>
2020-09-17 20:09:29 -0400

    style coming together for portfolio card

--- src/components/PortfolioCard.js
+++ src/components/PortfolioCard.js
@@ -4,9 +4,10 @@ import '../styles/portfolioCard.sass'
 
 export default function PortfolioCard({ still, embed, client }) {
     return (
-        <div>
+        <div className='portfolioCard'>
             <h2>{client}</h2>
             <Img fluid={still} alt={client} className='portfolioStill' />
+            <div className='portfolioOverlay'></div>
         </div>
     )
 }
