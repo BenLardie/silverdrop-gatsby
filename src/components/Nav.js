042c58ebc14e7b5e079170f0bdc738af0d3ece59
Ben Lardie <benlardie@gmail.com>
2020-09-24 19:55:18 -0400

    menu now closes after link is clicked

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react'
 import { Link } from "gatsby"
 import '../styles/nav.sass'
-import Burger from './Burger'
 
 export default function Nav() {
     if (typeof window !== "undefined") {
@@ -15,6 +14,7 @@ export default function Nav() {
         displayAddress: 'About'
     }, { to: '/#portfolio', displayAddress: 'Portfolio' }, { to: '/#contact', displayAddress: 'Contact' },]
 
+
     const navClick = () => {
         if (navState === '') {
             setNavState('open')
@@ -24,14 +24,26 @@ export default function Nav() {
             setNavOpen('')
         }
     }
+
+    const menuClick = () => {
+        if (navState === 'open') {
+            setNavState('')
+            setNavOpen('')
+            console.log('click')
+        } 
+    }
     return (
         <nav className={navOpen}>
+            <ul>
             {links.map((link, i=0) => {
                 i++
                 return (
-                    <Link to={link.to} className={`navLink ${navState}`} key={i}>{link.displayAddress}</Link>
+                    <li onClick={menuClick} key={i}>
+                    <Link to={link.to} className={`navLink ${navState}`}>{link.displayAddress}</Link>
+                    </li>
                 )
             } )}
+            </ul>
             <div className='hamburger'>
                 <div id="navIcon" className={navState} onClick={navClick}>
                     <span></span>
