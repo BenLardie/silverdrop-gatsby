b758255481c509ef69446e01f633d49cce53817b
Ben Lardie <benlardie@gmail.com>
2020-10-05 16:03:54 -0400

    iframe added not just giving it some props

--- src/components/PortfolioCard.js
+++ src/components/PortfolioCard.js
@@ -1,14 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Img from "gatsby-image"
 import '../styles/portfolioCard.sass'
+import Video from './Video'
 
 export default function PortfolioCard({ still, embed, title, client }) {
+    const [open, setOpen] = useState('closed')
     return (
         <div className='portfolioCard'>
             <h2>{title}</h2>
             <Img fluid={still} alt={client} className='portfolioStill' />
             <p className='portfolioBlurb'>{client}</p>
-            <i class="far fa-play-circle"></i>
+            <i class="far fa-play-circle" id='play'></i>
+            <Video embed={embed} open={open} />
         </div>
     )
 }
