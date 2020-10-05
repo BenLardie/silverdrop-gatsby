b470bf973a6e35dc34ed4ec34eedb97a5293bf61
Ben Lardie <benlardie@gmail.com>
2020-10-05 16:36:02 -0400

    module is now opening and closing

--- src/components/PortfolioCard.js
+++ src/components/PortfolioCard.js
@@ -4,13 +4,23 @@ import '../styles/portfolioCard.sass'
 import Video from './Video'
 
 export default function PortfolioCard({ still, embed, title, client }) {
-    const [open, setOpen] = useState('closed')
+    const [open, setOpen] = useState('hidden')
+    const openModule = () => {
+        if (open === 'hidden') {
+            setOpen('open')
+            console.log('open')
+        } else {
+            setOpen('hidden')
+        }
+    }
     return (
         <div className='portfolioCard'>
             <h2>{title}</h2>
             <Img fluid={still} alt={client} className='portfolioStill' />
             <p className='portfolioBlurb'>{client}</p>
-            <i class="far fa-play-circle" id='play'></i>
+            <span onClick={openModule}>
+            <i className="far fa-play-circle" id='play'></i>
+            </span>
             <Video embed={embed} open={open} />
         </div>
     )
--- src/components/Video.js
+++ src/components/Video.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import '../styles/video.sass'
 
-export default function Video() {
+export default function Video({ embed, open }) {
+    const url = embed
+    console.log(url)
     return (
-        <div>
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/ToaSW0bjMT4" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <div className={`videoModule ${open}`}>
+            <iframe 
+                width="560" 
+                height="315" 
+                src={url} 
+                frameBorder="0" 
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+                allowFullScreen
+                />
         </div>
     )
 }
